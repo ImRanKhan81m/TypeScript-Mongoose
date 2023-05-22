9c4d35c4ede615aef3e239512e31d2cc1eaaa485
Md Imran Hossen <freelancerimran81@gmail.com>
2023-05-22 23:01:52 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 5000
+
+// database connection
+
+async function bootstrap (){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/boosterbd', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        })
+        console.log('Database connected😎')
+    }catch(error){
+        console.log('Failed to connect database 😖', error)
+    }
+}
+
+bootstrap()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
